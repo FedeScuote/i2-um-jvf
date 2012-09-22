379e5fbafabc84bf23e39b994553ec89437d115d
fscuoteguazza@gmail.com <fscuoteguazza@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-09-22 00:22:44 +0000

    borrado para arreglar los src folders

--- trunk/proyecto/COMM/comm/ServiciosUsuario.java
+++
@@ -1,9 +0,0 @@
-package comm;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-import commExceptions.NoSeEncuentraUsuarioException;
-public interface ServiciosUsuario extends Remote{
-	public UsuarioVO login(String usuario,String contraseña) throws RemoteException, NoSeEncuentraUsuarioException;
-}
