19bee9a13d7681245aca9b8afe6cc47fdbc9a96a
vtuyare91 <vtuyare91@gmail.com>
2012-09-24 03:07:46 +0000

    Vamos que los tenemos!!!

--- proyecto/COMM/src/comm/ServiciosBatallaNaval.java
+++ proyecto/COMM/src/comm/ServiciosBatallaNaval.java
@@ -5,14 +5,14 @@ import java.rmi.RemoteException;
 import commExceptions.CoordenadasInvalidasException;
 
 public interface ServiciosBatallaNaval extends Remote {
-	//Metodo para agregar barcos al iniciar la partida.
-	public void agregarBarco(int nroTablero, int coordenadaInicialX, int coordenadaInicialY, int coordenadaFinalX, int coordenadaFinalY) throws RemoteException, CoordenadasInvalidasException;
+	//Metodo para agregar barcos al iniciar la partida
+	public void agregarBarco(UsuarioVO usuario, int coordenadaInicialX, int coordenadaInicialY, int coordenadaFinalX, int coordenadaFinalY) throws RemoteException, CoordenadasInvalidasException;
 
 	//Metodo para disparar
-	public void disparar(int nroTablero, int coordenadaX,int coordenadaY) throws RemoteException, CoordenadasInvalidasException;
+	public void disparar(UsuarioVO usuario, int coordenadaX,int coordenadaY) throws RemoteException, CoordenadasInvalidasException;
 
 	//Metodo para refrescar imagen de matriz
-	public TableroVO refrescarTablero(int nroTablero);
+	public TableroVO refrescarTablero(UsuarioVO usuario) throws RemoteException;
 
 
 
