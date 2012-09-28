5824733beb20517a805d9a80f0bdcb116acacb19
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-09-28 18:54:22 +0000

    Ya está implementado getRankingLudo

--- trunk/proyecto/DAO/src/daoImpl/RankingDAODB.java
+++ trunk/proyecto/DAO/src/daoImpl/RankingDAODB.java
@@ -40,61 +40,34 @@ public class RankingDAODB implements RankingDAO {
 	//construccion
 	public ArrayList getRankingLudo() throws NoHayRankingException{
 		ArrayList a=new ArrayList();
-		ArrayList b=new ArrayList();
+
+		ArrayList usuarios=new ArrayList();
+		ArrayList ganados=new ArrayList();
 
 
 		Conexion c=new Conexion("com.mysql.jdbc.Driver","jdbc:mysql://localhost/jvm", "root", "");
 
 		try {
 
-			ResultSet resultado = c.devolverResutado("SELECT DISTINCT juegos_idJuego, desafios_idDesafio, usuarioGanadorD FROM  `usuarios_has_juegos_desafios` WHERE juegos_idJuego='2'");
+			ResultSet resultado = c.devolverResutado("SELECT usuarios_idusuario, ganadas FROM  `ranking` WHERE juegos_idJuego='2'");
 			while (resultado.next()) {
-					Integer intObj=new Integer(resultado.getInt("usuarioGanadorD"));
-					b.add(intObj);
+					RankingDAODB r=new RankingDAODB();
+					int idUsuario=resultado.getInt("usuarios_idusuario");
+					String usuario=r.getUsuario(idUsuario);
+					//System.out.println(usuario);
+					r.setUsuario(usuario);
+					r.setGanadas(resultado.getInt("ganadas"));
+					a.add(r);
 			}
 
 		} catch (SQLException ex) {
 
 			throw new NoHayRankingException();
 
+		} catch (NoExisteUsuarioException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		for(int i=0;i<b.size();i++){
-			RankingDAODB r=new RankingDAODB();
-			String auxUsuario=null;
-			int auxGanadas=0;
-			//System.out.println("fila i="+i);
-			//System.out.println(b.size());
-			for(int j=0;j<b.size();j++){
-				int idUsuario=((Integer) b.get(i)).intValue();
-				//System.out.println("fila j="+j);
-				try {
-					auxUsuario=this.getUsuario(idUsuario);
-				} catch (NoExisteUsuarioException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				if(((Integer)b.get(j)).intValue()==(idUsuario)){
-					auxGanadas++;
-				}
-			}
-
-			r.setUsuario(auxUsuario);
-			r.setGanadas(auxGanadas);
-			a.add(r);
-
-
-		}
-		HashSet hs=new HashSet();
-		hs.addAll(a);
-		a.clear();
-		a.addAll(hs);
-
-
-
-
-
-
 
 
 
@@ -117,7 +90,7 @@ public class RankingDAODB implements RankingDAO {
 			throw new NoExisteUsuarioException();
 
 		}
-		c.disconnect();
+		//c.disconnect();
 		return usuario;
 	}
 
