9acd56835945b81ebaaf8da948d45e2c56d5705e
vtuyare91@gmail.com <vtuyare91@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-09-28 21:23:28 +0000

    Controlamos contraseña

--- trunk/proyecto/BUS/src/busImpl/BusServer.java
+++ trunk/proyecto/BUS/src/busImpl/BusServer.java
@@ -4,15 +4,20 @@ import java.rmi.RemoteException;
 
 import comm.ServiciosUsuario;
 import comm.UsuarioVO;
+import commExceptions.ContrasenaInvalidaException;
 import commExceptions.NoSeEncuentraUsuarioException;
 
 public class BusServer implements ServiciosUsuario {
 	// implemento mi logear
-	public UsuarioVO login(String usuario, String contraseña) throws RemoteException,NoSeEncuentraUsuarioException {
+	public UsuarioVO login(String usuario, String contraseña) throws RemoteException,NoSeEncuentraUsuarioException,ContrasenaInvalidaException {
 
 		try {
 			Usuario nuevo = Usuario.findByName(usuario);
-			return new UsuarioVO(nuevo.getNombreB(), nuevo.getApellidoB());
+			if(nuevo.getClaveB().equals(contraseña)){
+				return new UsuarioVO(nuevo.getNombreB(), nuevo.getApellidoB());
+			}else{
+				throw new ContrasenaInvalidaException();
+			}
 		} catch (NoSeEncuentraUsuarioException e) {
 			throw new NoSeEncuentraUsuarioException();
 		}
