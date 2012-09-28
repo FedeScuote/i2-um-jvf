e59791c61f509a9c3842d2abbc0801f1a99d0271
Federico Scuoteguazza <FedeScuote@users.noreply.github.com>
2012-09-28 13:57:05 +0000

    Creado RankingVO y modificado ServiciosRanking

---
+++ proyecto/COMM/src/comm/RankingVO.java
@@ -0,0 +1,28 @@
+package comm;
+
+import java.io.Serializable;
+
+public class RankingVO implements Serializable {
+
+	public Integer nro;
+	public String usuario;
+
+	public int getNro() {
+		return nro;
+	}
+
+	public void setNro(int nro) {
+		this.nro = nro;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String nroAString(){
+		return nro.toString();
+	}
+}
