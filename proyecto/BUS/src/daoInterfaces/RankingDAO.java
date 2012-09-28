d40ebd53e71293d22563252074c9aa057207803c
john.hirata.personal <john.hirata.personal@gmail.com>
2012-09-28 19:02:28 +0000

    Agregué las firmas de Ranking de BatallaNaval y la de Backgammon.

--- proyecto/BUS/src/daoInterfaces/RankingDAO.java
+++ proyecto/BUS/src/daoInterfaces/RankingDAO.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import excepcionesB.NoHayRankingException;
 
 public interface RankingDAO {
-	//construcción
+	//terminado,
 	public ArrayList getRankingLudo()throws NoHayRankingException;
+
+	//Construcción
+	public ArrayList getRankingBatallaNaval()throws NoHayRankingException;
+	public ArrayList getRankingBackgammon()throws NoHayRankingException;
+
 }
