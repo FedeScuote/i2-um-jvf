6943094187a7618e966c16f0afaf0a2357647aae
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-09-28 19:08:06 +0000

    Terminé de implementar Rankings

--- trunk/proyecto/BUS/src/daoInterfaces/RankingDAO.java
+++ trunk/proyecto/BUS/src/daoInterfaces/RankingDAO.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import excepcionesB.NoHayRankingException;
 
 public interface RankingDAO {
-	//terminado,
+	//terminado, es una colección de objetos del tipo RankingDAODB
 	public ArrayList getRankingLudo()throws NoHayRankingException;
-
-	//Construcción
+	//terminado es una colección de objetos del tipo RankingDAODB
 	public ArrayList getRankingBatallaNaval()throws NoHayRankingException;
+	//terminado es una colección de objetos del tipo RankingDAODB
 	public ArrayList getRankingBackgammon()throws NoHayRankingException;
 
 }
