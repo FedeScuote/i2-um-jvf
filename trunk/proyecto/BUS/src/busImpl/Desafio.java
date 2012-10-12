6e53c24c90130528bbb18d08545100edaeea7dc2
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-10-12 16:39:20 +0000

    Agregué la clase Desafio en Negocio.

---
+++ trunk/proyecto/BUS/src/busImpl/Desafio.java
@@ -0,0 +1,37 @@
+package busImpl;
+
+import java.util.Date;
+
+public class Desafio {
+
+	private int idDesafio;
+	private int monto;
+	private Date fechaHoraInicioD;
+	private String estado; //En hora, En hora-lleno, En curso, Atrasado, Finalizado, Cancelado
+	public int getIdDesafio() {
+		return idDesafio;
+	}
+	public void setIdDesafio(int idDesafio) {
+		this.idDesafio = idDesafio;
+	}
+	public int getMonto() {
+		return monto;
+	}
+	public void setMonto(int monto) {
+		this.monto = monto;
+	}
+	public Date getFechaHoraInicioD() {
+		return fechaHoraInicioD;
+	}
+	public void setFechaHoraInicioD(Date fechaHoraInicioD) {
+		this.fechaHoraInicioD = fechaHoraInicioD;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+
+}
