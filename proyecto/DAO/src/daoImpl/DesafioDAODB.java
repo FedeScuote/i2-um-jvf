e44b10ad9d4aff20cfa72ff510e5f67ac8654bf4
john.hirata.personal <john.hirata.personal@gmail.com>
2012-10-12 16:40:23 +0000

    Implementé el método que te da los Desafios disponibles. Falta probarlo.

--- proyecto/DAO/src/daoImpl/DesafioDAODB.java
+++ proyecto/DAO/src/daoImpl/DesafioDAODB.java
@@ -1,9 +1,11 @@
 package daoImpl;
 
+import java.util.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import busImpl.Desafio;
 import busImpl.Ranking;
 import conexion.Conexion;
 
@@ -14,29 +16,67 @@ import excepcionesD.NoExisteUsuarioException;
 
 public class DesafioDAODB implements DesafioDAO {
 
-	private int desafiosDisponibles=0;
+	private int idDesafio;
+	private int monto;
+	private Date fechaHoraInicioD;
+	private String estado; //En hora, En hora-lleno, En curso, Atrasado, Finalizado, Cancelado
+
+	public int getIdDesafio() {
+		return idDesafio;
+	}
+
+	public void setIdDesafio(int idDesafio) {
+		this.idDesafio = idDesafio;
+	}
+
+	public int getMonto() {
+		return monto;
+	}
+
+	public void setMonto(int monto) {
+		this.monto = monto;
+	}
+
+	public Date getFechaHoraInicioD() {
+		return fechaHoraInicioD;
+	}
+
+	public void setFechaHoraInicioD(Date fechaHoraInicioD) {
+		this.fechaHoraInicioD = fechaHoraInicioD;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 
 	public ArrayList getDesafios() throws NoHayDesafioException {
 		ArrayList a=new ArrayList();
 
-		ArrayList usuarios=new ArrayList();
-		ArrayList ganados=new ArrayList();
+
 
 
 		Conexion c=new Conexion("com.mysql.jdbc.Driver","jdbc:mysql://localhost/jvm", "root", "");
 
 		try {
 
-			ResultSet resultado = c.devolverResutado("SELECT usuarios_idusuario, ganadas FROM  `ranking` WHERE juegos_idJuego='2' ORDER BY ganadas DESC");
+			ResultSet resultado = c.devolverResutado("SELECT idDesafio, monto, fechaHoraInicioD, estadoD FROM desafios WHERE fechaHoraInicioD >= now() ");
 			while (resultado.next()) {
-					RankingDAODB r1=new RankingDAODB();
-					Ranking r=new Ranking();
-					int idUsuario=resultado.getInt("usuarios_idusuario");
-					String usuario=r1.getUsuario(idUsuario);
+					Desafio d=new Desafio();
+
+					int idDesafio=resultado.getInt("idDesafio");
+					int monto=resultado.getInt("monto");
+					Date fecha=resultado.getDate("fechaHoraInicioD");
+					String estadoD=resultado.getString("estadoD");
 					//System.out.println(usuario);
-					r.setUsuario(usuario);
-					r.setGanadas(resultado.getInt("ganadas"));
-					a.add(r);
+					d.setIdDesafio(idDesafio);
+					d.setMonto(monto);
+					d.setFechaHoraInicioD(fecha);
+					d.setEstado(estadoD);
+					a.add(d);
 			}
 			a.get(0);
 
@@ -44,9 +84,6 @@ public class DesafioDAODB implements DesafioDAO {
 
 			throw new NoHayDesafioException();
 
-		} catch (NoExisteUsuarioException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IndexOutOfBoundsException i) {
 			throw new NoHayDesafioException();
 		}
