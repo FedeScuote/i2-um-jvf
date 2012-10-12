600ba028b9f8888193703574b623cf93ff8ff4f8
Federico Scuoteguazza <FedeScuote@users.noreply.github.com>
2012-10-12 21:04:24 +0000

    ToString desafioBatallaNavalVO

--- proyecto/COMM/src/comm/DesafioBatallaNavalVO.java
+++ proyecto/COMM/src/comm/DesafioBatallaNavalVO.java
@@ -6,4 +6,12 @@ public class DesafioBatallaNavalVO extends DesafioVO implements Serializable{
 
 	final private String Juego = "BATALLA NAVAL";
 
+	@Override
+	public String toString() {
+
+		return ((Integer)this.getApuesta()).toString();
+	}
+
+
+
 }
