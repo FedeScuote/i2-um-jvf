854ec3db977e6ea042b3d0c9a28bdfffd43446d8
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-10-16 03:46:51 +0000

    corregí los métodos, ahora son del tipo ranking.

--- trunk/proyecto/BUS/src/daoInterfaces/RankingDAO.java
+++ trunk/proyecto/BUS/src/daoInterfaces/RankingDAO.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import excepcionesB.NoHayRankingException;
 
 public interface RankingDAO {
-	//terminado, es una colección de objetos del tipo RankingDAODB
+	//terminado, es una colección de objetos del tipo Ranking
 	public ArrayList getRankingLudo()throws NoHayRankingException;
-	//terminado es una colección de objetos del tipo RankingDAODB
+	//terminado es una colección de objetos del tipo Ranking
 	public ArrayList getRankingBatallaNaval()throws NoHayRankingException;
-	//terminado es una colección de objetos del tipo RankingDAODB
+	//terminado es una colección de objetos del tipo Ranking
 	public ArrayList getRankingBackgammon()throws NoHayRankingException;
-	//terminado  es una colección de objetos del tipo RankingDAODB
+	//terminado  es una colección de objetos del tipo Ranking
 	public ArrayList getRankingGeneral() throws NoHayRankingException;
 
 }
