1b976e9054b5cb032fac0103fdd2756bc6a0ff1a
john.hirata.personal <john.hirata.personal@gmail.com>
2012-10-21 15:45:32 +0000

    agregué un atributo

---
+++ proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
@@ -0,0 +1,19 @@
+package daoInterfaces;
+
+import java.util.ArrayList;
+
+import busImpl.Disparo;
+import busImpl.Estados;
+import busImpl.RegistroDisparo;
+import busImpl.Tablero;
+
+public interface BatallaNavalDAO {
+
+	public ArrayList<RegistroDisparo> getListaDeTiros(int idPartida,int idUsuario);
+	public Tablero getTablero(int idPartidia,int idUsuario);
+
+	public void registrarDisparo(Disparo disparo, Estados estado, int idUsuario);
+	public void regstrarTablero(Tablero tablero);
+
+
+}
