78808234690f56387b110f59c8943dd34b5be660
john.hirata.personal <john.hirata.personal@gmail.com>
2012-10-21 21:07:03 +0000

    Algunos comentarios

--- proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
+++ proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
@@ -12,6 +12,7 @@ public interface BatallaNavalDAO {
 	public ArrayList<RegistroDisparo> getListaDeTiros(int idPartida,int idUsuario);
 	public Tablero getTablero(int idPartidia,int idUsuario);
 
+	//terminados
 	public void registrarDisparo(Disparo disparo, Estados estado, int idUsuario, int idPartida);
 	public void regstrarTablero(Tablero tablero, int idPartida);
 
