e4fe94f2abc3c840cfff23d64ae7beffa2b7ba69
vtuyare91 <vtuyare91@gmail.com>
2012-10-22 22:34:40 +0000

    se agrega metodo a la interfaz dao para obtener si un usario tiene una partida pendiente o no

--- proyecto/BUS/src/daoInterfaces/PartidaDAO.java
+++ proyecto/BUS/src/daoInterfaces/PartidaDAO.java
@@ -3,5 +3,7 @@ package daoInterfaces;
 public interface PartidaDAO {
 	//terminado, devuelve idPartida
 	public int concretarDesafio(int idDesafio,int idDesafiante);
+	//devuelve true en caso de que el usuario tenga una partida pendiente, false en caso contrario
+	public boolean partidaPendiente(int idUsuario);
 
 }
