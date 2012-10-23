1edf768f77ef78d3a479c57d344dbdd4b67c9532
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-10-23 05:17:26 +0000

    Terminé de implementar las interfaces, falta probarlos más veces.

--- trunk/proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
+++ trunk/proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
@@ -8,11 +8,9 @@ import busImpl.RegistroDisparo;
 import busImpl.Tablero;
 
 public interface BatallaNavalDAO {
-	//construcción
-
-	public ArrayList<RegistroDisparo> getListaDeTiros(int idPartida,int idUsuario);
 
 	//terminados
+	public ArrayList<RegistroDisparo> getListaDeTiros(int idPartida,int idUsuario);
 	public void registrarDisparo(Disparo disparo, Estados estado, int idUsuario, int idPartida);
 	public Tablero getTablero(int idPartidia,int idUsuario);
 	public void regstrarTablero(Tablero tablero, int idPartida);
