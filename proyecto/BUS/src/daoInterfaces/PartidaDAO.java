73a83bfb4d82a6118c68cfb716d94db56c1f27a3
john.hirata.personal <john.hirata.personal@gmail.com>
2012-10-23 16:24:58 +0000

    Terminado las impelentaciones de Batalla Naval

--- proyecto/BUS/src/daoInterfaces/PartidaDAO.java
+++ proyecto/BUS/src/daoInterfaces/PartidaDAO.java
@@ -5,9 +5,10 @@ import busImpl.Usuario;
 public interface PartidaDAO {
 	//terminado, devuelve idPartida
 	public int concretarDesafio(int idDesafio,int idDesafiante);
-	//devuelve true en caso de que el usuario tenga una partida pendiente, false en caso contrario
+	//terminado, devuelve true en caso de que el usuario tenga una partida pendiente, false en caso contrario
 	public boolean partidaPendiente(int idUsuario);
-
+	//terminado, devuelve la idPartida o retorna cero si no existe
 	public int idPartida(int idUsuario);
+	//terminado, devuelve el Usuario oponente o retorna null
 	public Usuario oponente(int idUsuario);
 }
