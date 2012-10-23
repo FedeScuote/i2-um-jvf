0f69bf8ded7de68ca5480ec9b173354813ddcf8a
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-10-23 03:37:24 +0000

    Se terminaron de implementar 3 métodos de la interface

--- trunk/proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
+++ trunk/proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
@@ -8,13 +8,14 @@ import busImpl.RegistroDisparo;
 import busImpl.Tablero;
 
 public interface BatallaNavalDAO {
+	//construcción
 
 	public ArrayList<RegistroDisparo> getListaDeTiros(int idPartida,int idUsuario);
-	//construcción
-	public Tablero getTablero(int idPartidia,int idUsuario); //falta las celdas
-	public void regstrarTablero(Tablero tablero, int idPartida); //falta las celdas
+
 	//terminados
 	public void registrarDisparo(Disparo disparo, Estados estado, int idUsuario, int idPartida);
+	public Tablero getTablero(int idPartidia,int idUsuario);
+	public void regstrarTablero(Tablero tablero, int idPartida);
 
 
 
