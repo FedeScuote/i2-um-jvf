a82878546a2f0df02a35915ec584bd21373d5bae
vtuyare91@gmail.com <vtuyare91@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-10-25 02:44:52 +0000

    Se agrega metodo a los servicios desafios brindados a la capa de presentacion para poder jugar luego de que un desafio se alla aceptado, es decir pasado de ser un desafio a una partida en curso

--- trunk/proyecto/COMM/src/comm/ServiciosDesafio.java
+++ trunk/proyecto/COMM/src/comm/ServiciosDesafio.java
@@ -9,5 +9,5 @@ import commExceptions.NoHayDesafiosDisponiblesException;
 public interface ServiciosDesafio extends Remote{
 
 	public ArrayList<DesafioBatallaNavalVO> getDesafios() throws RemoteException, NoHayDesafiosDisponiblesException;
-
+	//public boolean aceptaronDesafio(UsuarioVO usuario) throws RemoteException;
 }
