593a46db1d252a7d25492877e14f8b75e1bedf63
john.hirata.personal <john.hirata.personal@gmail.com>
2012-10-25 23:01:27 +0000

    Hice la el método actualizar Tablero en la interface Batalla naval

--- proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
+++ proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
@@ -14,7 +14,7 @@ public interface BatallaNavalDAO {
 	public void registrarDisparo(Disparo disparo, Estados estado, int idUsuario, int idPartida);
 	public Tablero getTablero(int idPartidia,int idUsuario);
 	public void regstrarTablero(Tablero tablero, int idPartida);
-	
+	public void actualizarTablero(int idPartida, String usuario, boolean miTurno,int barcosSubmarinos, int barcosDestructores,int barcosCruceros,int barcosAcorazados,int barcosSubmarinosColocados, int barcosDestructoresColocados,int barcosCrucerosColocados,int barcosAcorazadosColocados );
 
 
 }
