c0536a9d480cfd35894799669f0de2006d01845a
fscuoteguazza <fscuoteguazza@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-10-26 17:46:15 +0000

    implementacion de esperar si me aceptaron desafio

--- trunk/proyecto/GuiObligatorio/src/pantallaElegirDesafioOJuego/SegundaPantallaBatallaN.java
+++ trunk/proyecto/GuiObligatorio/src/pantallaElegirDesafioOJuego/SegundaPantallaBatallaN.java
@@ -1,6 +1,9 @@
 package pantallaElegirDesafioOJuego;
 
+import java.awt.Color;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -12,8 +15,10 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import javax.swing.Timer;
 import javax.swing.table.DefaultTableModel;
 
+import util.EstadoElegirDesafio;
 import ventanaJuego.ColocarBarcosVentana;
 
 import comm.DesafioBatallaNavalVO;
@@ -24,17 +29,48 @@ import comm.ServiciosDesafio;
 import comm.ServiciosRanking;
 import comm.UsuarioVO;
 import commExceptions.NoHayDesafiosDisponiblesException;
+import commExceptions.NoSeEncuentraUsuarioException;
 
 public class SegundaPantallaBatallaN extends SegundaPantalla {
 
 	private final static String host = null;
 
-	private ArrayList<DesafioBatallaNavalVO> arrayDesafio= null;
+	private final static int pause = 10000;
 
-	public SegundaPantallaBatallaN(UsuarioVO usuario) {
+	private ArrayList<DesafioBatallaNavalVO> arrayDesafio = null;
+
+	private Timer temporizador;
+
+	public SegundaPantallaBatallaN(final UsuarioVO usuario) {
 		super();
 		this.llenarListaDesafio();
-		this.usuario=usuario;
+		this.usuario = usuario;
+		ActionListener taskPerformer = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				try { // intento recibir datos para el ranking/
+					Registry registry = LocateRegistry.getRegistry(host);
+					ServiciosDesafio stub = (ServiciosDesafio) registry
+							.lookup("Desafio");
+//					if(stub.aceptaronDesafio(usuario)){
+//						temporizador.stop();
+//						iniciarPartida
+//					}
+
+				}
+				catch (Exception remoteExceptionrmi) {
+					if (remoteExceptionrmi instanceof NoSeEncuentraUsuarioException) {
+						System.out.println("no existe usuario");
+					} else {
+						System.err.println("Client exception: "
+								+ remoteExceptionrmi.toString());
+						remoteExceptionrmi.printStackTrace();
+					}
+			}
+			}
+		};
+		temporizador = new Timer(pause, taskPerformer);
+		temporizador.setInitialDelay(pause);
+		temporizador.addActionListener(taskPerformer);
 	}
 
 	private void llenarListaDesafio() {
@@ -55,7 +91,8 @@ public class SegundaPantallaBatallaN extends SegundaPantalla {
 
 	// METODO PARA LLENAR UNA JTABLE CON UN ARRAY DE OBJETOS
 	private void llenarTabla(JTable tabla,
-			ArrayList<DesafioBatallaNavalVO> lista, final SegundaPantallaBatallaN pantalla) {
+			ArrayList<DesafioBatallaNavalVO> lista,
+			final SegundaPantallaBatallaN pantalla) {
 		DefaultTableModel model = new DefaultTableModel() { // me hago mi modelo
 			// para que no puedan editar la tabla
 			@Override
@@ -72,40 +109,70 @@ public class SegundaPantallaBatallaN extends SegundaPantalla {
 			DesafioBatallaNavalVO rank = (DesafioBatallaNavalVO) i.next();
 			model.addRow(new String[] {
 					((Integer) rank.getApuesta()).toString(),
-					( rank.getUsuario().getUsuarioB()) });
+					(rank.getUsuario().getUsuarioB()) });
 		}
 
-		//añado mi action listener para ver aceptar un desafio
-		desafiosDisponiblesTabla.addMouseListener(new java.awt.event.MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-			    if (e.getClickCount() == 2) {
-			      JTable target = (JTable)e.getSource();
-			      int row = target.getSelectedRow();
-			      pantalla.iniciarPartida(pantalla.arrayDesafio.get(row), pantalla.usuario);
-			      pantalla.dispose();
-			      ColocarBarcosVentana l = new ColocarBarcosVentana(pantalla.usuario);
-			      l.setVisible(true);
-			    }
-			  }
-		});
+		// añado mi action listener para ver aceptar un desafio
+		desafiosDisponiblesTabla
+				.addMouseListener(new java.awt.event.MouseAdapter() {
+					public void mouseClicked(MouseEvent e) {
+						if (e.getClickCount() == 2) {
+							JTable target = (JTable) e.getSource();
+							int row = target.getSelectedRow();
+							pantalla.iniciarPartida(pantalla.arrayDesafio
+									.get(row), pantalla.usuario);
+							pantalla.dispose();
+							ColocarBarcosVentana l = new ColocarBarcosVentana(
+									pantalla.usuario);
+							l.setVisible(true);
+						}
+					}
+				});
 
 	}
-	//metodo para iniciar mi partida
-	private void iniciarPartida(DesafioBatallaNavalVO desafio, UsuarioVO desafiante){
+
+	// metodo para iniciar mi partida
+	private void iniciarPartida(DesafioBatallaNavalVO desafio,
+			UsuarioVO desafiante) {
 		try { // intento recibir datos para el ranking
 			Registry registry = LocateRegistry.getRegistry(host);
-			ServiciosBatallaNaval stub = (ServiciosBatallaNaval) registry.lookup("BatallaNavalServices");
+			ServiciosBatallaNaval stub = (ServiciosBatallaNaval) registry
+					.lookup("BatallaNavalServices");
 			stub.iniciarPartida(desafio, desafiante);
-		}catch(Exception e){
-			if(e instanceof RemoteException){
-				JOptionPane.showMessageDialog(new JFrame(),"Error en la conexion intente de nuevo", "ERROR", JOptionPane.ERROR_MESSAGE);
-			}else{
+		} catch (Exception e) {
+			if (e instanceof RemoteException) {
+				JOptionPane.showMessageDialog(new JFrame(),
+						"Error en la conexion intente de nuevo", "ERROR",
+						JOptionPane.ERROR_MESSAGE);
+			} else {
 				e.printStackTrace();
-				JOptionPane.showMessageDialog(new JFrame(),"ERROR DESCONOCIDO", "ERROR", JOptionPane.ERROR_MESSAGE);
+				JOptionPane
+						.showMessageDialog(new JFrame(), "ERROR DESCONOCIDO",
+								"ERROR", JOptionPane.ERROR_MESSAGE);
 				this.dispose();
 			}
 		}
 	}
 
+	private void crearDesafio(UsuarioVO desafiante) {
+		try { // intento recibir datos para el ranking/
+			Registry registry = LocateRegistry.getRegistry(host);
+			ServiciosDesafio stub = (ServiciosDesafio) registry
+					.lookup("Desafio");
+			//stub.crearDesafio(this.usuario);
+			temporizador.start();
+		}
+		catch (Exception remoteExceptionrmi) {
+			if (remoteExceptionrmi instanceof NoSeEncuentraUsuarioException) {
+				System.out.println("no existe usuario");
+			} else {
+				System.err.println("Client exception: "
+						+ remoteExceptionrmi.toString());
+				remoteExceptionrmi.printStackTrace();
+			}
+
+		}
+
+	}
 
 }
