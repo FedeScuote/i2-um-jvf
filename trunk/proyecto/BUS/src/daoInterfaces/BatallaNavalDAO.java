9e65e503c239830ef81fb52c073516f3bb993fc3
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-10-27 03:46:17 +0000

    corregí el nombre de un método, registrarTablero

--- trunk/proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
+++ trunk/proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
@@ -13,7 +13,7 @@ public interface BatallaNavalDAO {
 	public ArrayList<RegistroDisparo> getListaDeTiros(int idPartida,int idUsuario);
 	public void registrarDisparo(Disparo disparo, Estados estado, int idUsuario, int idPartida);
 	public Tablero getTablero(int idPartidia,int idUsuario);
-	public void regstrarTablero(Tablero tablero, int idPartida);
+	public void registrarTablero(Tablero tablero, int idPartida);
 	public void actualizarTablero(int idPartida, String usuario, boolean miTurno,int barcosSubmarinos, int barcosDestructores,int barcosCruceros,int barcosAcorazados,int barcosSubmarinosColocados, int barcosDestructoresColocados,int barcosCrucerosColocados,int barcosAcorazadosColocados );
 
 
