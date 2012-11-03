869d682098648dcedf26f9ad0e9a1017fe24a9d8
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-03 22:00:45 +0000

    Agregué una excepción

--- trunk/proyecto/BUS/src/daoInterfaces/DesafioDAOLudo.java
+++ trunk/proyecto/BUS/src/daoInterfaces/DesafioDAOLudo.java
@@ -5,10 +5,12 @@ import excepcionesB.NoTieneDesafioException;
 import excepcionesB.YaTieneOtroDesafioException;
 
 public interface DesafioDAOLudo {
-	//Se crea desafio siempre y cuando haya credito disponiblre
+	//terminado sin probar, se crea desafio siempre y cuando haya credito disponible
 	public void crearDesafioLudo(int idUsuario, int monto)throws NoHaySuficienteCreditoUsuarioException;
+
 	//cada usuario va aceptando desafio hasta que hayan cuatro jugadores en el ludo
 	public void aceptoDesafioLudo(int idUsuario, int idDesafio)throws YaTieneOtroDesafioException, NoHaySuficienteCreditoUsuarioException;
+
 	//devuelve true cuando ya se inició una partida
 	public boolean inicioDesafioLudo(int idUsuario)throws NoTieneDesafioException;
 
