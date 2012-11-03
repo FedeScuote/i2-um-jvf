4e5fb169f33684d1620cb4b007faf566c6a9e2d9
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-03 19:32:28 +0000

    Agrego Excepción a un método.

--- trunk/proyecto/BUS/src/daoInterfaces/DesafioDAOLudo.java
+++ trunk/proyecto/BUS/src/daoInterfaces/DesafioDAOLudo.java
@@ -6,10 +6,10 @@ import excepcionesB.YaTieneOtroDesafioException;
 
 public interface DesafioDAOLudo {
 	//Se crea desafio siempre y cuando haya credito disponiblre
-	public void crearDesafio(int idUsuario, int monto)throws NoHaySuficienteCreditoUsuarioException;
+	public void crearDesafioLudo(int idUsuario, int monto)throws NoHaySuficienteCreditoUsuarioException;
 	//cada usuario va aceptando desafio hasta que hayan cuatro jugadores en el ludo
 	public void aceptoDesafioLudo(int idUsuario, int idDesafio)throws YaTieneOtroDesafioException, NoHaySuficienteCreditoUsuarioException;
 	//devuelve true cuando ya se inició una partida
-	public boolean inicioDesafio(int idUsuario)throws NoTieneDesafioException;
+	public boolean inicioDesafioLudo(int idUsuario)throws NoTieneDesafioException;
 
 }
