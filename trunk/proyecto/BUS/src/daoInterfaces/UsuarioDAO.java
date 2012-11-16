fa043daab111b9a6eef2d1e6885c701a293946e0
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-16 18:16:10 +0000

    Actualizando interfaces y excepciones

--- trunk/proyecto/BUS/src/daoInterfaces/UsuarioDAO.java
+++ trunk/proyecto/BUS/src/daoInterfaces/UsuarioDAO.java
@@ -3,21 +3,33 @@ package daoInterfaces;
 
 import java.util.ArrayList;
 
+import excepcionesB.NoExisteUsuarioException;
 import excepcionesB.NotDataFoundException;
 import excepcionesB.YaExisteUsuarioException;
 import busImpl.Usuario;
 
 
 public interface UsuarioDAO {
-	//Implementado
+	//terminado y probado
 	public Usuario findByName(String usuario) throws NotDataFoundException;
 
-	//No implementado
+	//terminado y probado
 	public boolean agregarUsuario(String usuario, String clave, int nivelPrilegio, int virtual, int credito, int partidasGanadas, String nombre, String apellido, String pais) throws YaExisteUsuarioException;
 
 	//terminados y probados
 	public ArrayList<Usuario> getUsuariosVirtuales();
 	public boolean esUsuarioVirtual(String usuario);
+
+	//terminados y probados, el crédito a restar o sumar debe ser un valor positivo, esto devuelve el resultado
+	public int restarSaldo(int credito_a_restar, int idUsuario);
+	public int sumarSaldo(int credito_a_sumar, int idUsuario);
+
+	//terminado y probado, devuelve true si el usuario dispone del credito que se solicita
+	public boolean creditoSuficiente(int credito, int idUsuario);
+
+	//terminados y probados
+	public boolean cambiarNombre(String usuario, String nuevoUsuario) throws NoExisteUsuarioException;
+	public boolean cambiarPassword(String usuario, String nuevaPassword)throws NoExisteUsuarioException;
 
 
 
