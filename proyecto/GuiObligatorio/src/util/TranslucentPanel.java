5dc5d4e53c2a57e6783bf708b0838d38a3c78c38
Federico Scuoteguazza <FedeScuote@users.noreply.github.com>
2012-11-17 20:50:06 +0000

    Arreglo bug en el panel interior de batallaNaval

--- proyecto/GuiObligatorio/src/util/TranslucentPanel.java
+++ proyecto/GuiObligatorio/src/util/TranslucentPanel.java
@@ -10,12 +10,14 @@ public class TranslucentPanel extends JPanel {
 
 	public TranslucentPanel() {
 		super();
+		this.setOpaque(false);
 		Color translucent = new Color(0, 0, 0, 0.5f);
 		this.setBackground(translucent);
 	}
 
 	public TranslucentPanel(Color color) {
 		super();
+		this.setOpaque(false);
 		Color translucent = new Color(color.getRed(), color.getGreen(), color
 				.getBlue(), 128);
 		this.setBackground(translucent);
