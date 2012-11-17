60fdf67f1ce4c0edd217b375d25182e69d05d1aa
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-17 22:00:13 +0000

    actualicé las interfaces y modifiqué las implementaciones

--- trunk/proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
+++ trunk/proyecto/BUS/src/daoInterfaces/BatallaNavalDAO.java
@@ -11,11 +11,11 @@ import busImpl.batallaNaval.Tablero;
 public interface BatallaNavalDAO {
 
 	//terminados y optimizados
-	public ArrayList<RegistroDisparo> getListaDeTiros(int idPartida,int idUsuario);
-	public void registrarDisparo(Disparo disparo, Estados estado, int idUsuario, int idPartida);
-	public Tablero getTablero(int idPartidia,int idUsuario);
+	public ArrayList<RegistroDisparo> getListaDeTiros(int idUsuario);
+	public void registrarDisparo(Disparo disparo, Estados estado, int idUsuario);
+	public Tablero getTablero(int idUsuario);
 	public void registrarTablero(Tablero tablero, int idPartida);
-	public void actualizarTablero(int idPartida, String usuario, boolean miTurno,int barcosSubmarinos, int barcosDestructores,int barcosCruceros,int barcosAcorazados,int barcosSubmarinosColocados, int barcosDestructoresColocados,int barcosCrucerosColocados,int barcosAcorazadosColocados );
+	public void actualizarTablero(int idUsuario, boolean miTurno,int barcosSubmarinos, int barcosDestructores,int barcosCruceros,int barcosAcorazados,int barcosSubmarinosColocados, int barcosDestructoresColocados,int barcosCrucerosColocados,int barcosAcorazadosColocados );
 	public void modificarCeldaTablero(int idUsuario, Celda celda, int xC, int yC);
 	public boolean turnoTablero(int idUsuario);
 
