7c21a7f4bb1ceabd4691189b30f0f6d76c2b04ed
john.hirata.personal@gmail.com <john.hirata.personal@gmail.com@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-18 22:46:23 +0000

    agrego Reporte

---
+++ trunk/proyecto/BUS/src/busImpl/Reporte.java
@@ -0,0 +1,29 @@
+package busImpl;
+
+public class Reporte {
+	private int montoTotalNoVirtuales=0;
+	private int montoTotalVirtuales=0;
+	private int montoTotalComisionesJVF=0;
+
+
+	public int getMontoTotalNoVirtuales() {
+		return montoTotalNoVirtuales;
+	}
+	public void setMontoTotalNoVirtuales(int montoTotalNoVirtuales) {
+		this.montoTotalNoVirtuales = montoTotalNoVirtuales;
+	}
+	public int getMontoTotalVirtuales() {
+		return montoTotalVirtuales;
+	}
+	public void setMontoTotalVirtuales(int montoTotalVirtuales) {
+		this.montoTotalVirtuales = montoTotalVirtuales;
+	}
+	public int getMontoTotalComisionesJVF() {
+		return montoTotalComisionesJVF;
+	}
+	public void setMontoTotalComisionesJVF(int montoTotalComisionesJVF) {
+		this.montoTotalComisionesJVF = montoTotalComisionesJVF;
+	}
+
+
+}
