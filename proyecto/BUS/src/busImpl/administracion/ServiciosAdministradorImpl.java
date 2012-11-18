7066d76865c01d9519d5d02b712c508aa55b8416
vtuyare91 <vtuyare91@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-18 13:43:44 +0000

    Se implementan los metodos de la intefaz

--- proyecto/BUS/src/busImpl/administracion/ServiciosAdministradorImpl.java
+++ proyecto/BUS/src/busImpl/administracion/ServiciosAdministradorImpl.java
@@ -1,5 +1,137 @@
 package busImpl.administracion;
 
-public class ServiciosAdministradorImpl {
+import java.rmi.RemoteException;
+
+import busImpl.Usuario;
+
+import comm.ServiciosAdministrador;
+import comm.UsuarioVO;
+import commExceptions.ContrasenaInvalidaException;
+import commExceptions.MontoInsuficienteException;
+import commExceptions.NoSeEncuentraUsuarioException;
+import commExceptions.UsuarioDuplicadoException;
+import daoInterfaces.UsuarioDAO;
+import excepcionesB.NoExisteUsuarioException;
+import excepcionesB.NotDataFoundException;
+import excepcionesB.YaExisteUsuarioException;
+
+public class ServiciosAdministradorImpl implements ServiciosAdministrador{
+
+	public void acreditarSaldo(String usuario, int credito) throws RemoteException, NoSeEncuentraUsuarioException {
+		UsuarioDAO daoUsuario = getUsuarioDAO();
+		try {
+			Usuario user=daoUsuario.findByName(usuario);
+			daoUsuario.sumarSaldo(credito, user.getIdUsuarioB());
+		} catch (NotDataFoundException e) {
+			throw new NoSeEncuentraUsuarioException();
+		}
+
+	}
+
+	public void agregarUsuario(String usuario, String clave, int nivelPrilegio, String nombre, String apellido, String pais) throws RemoteException, UsuarioDuplicadoException {
+		UsuarioDAO daoUsuario = getUsuarioDAO();
+		try {
+			daoUsuario.agregarUsuario(usuario, clave, nivelPrilegio, 0, 100, 0, nombre, apellido, pais);
+		} catch (YaExisteUsuarioException e) {
+			throw new UsuarioDuplicadoException();
+		}
+
+	}
+
+	public void cambiarNombre(String usuario, String password, String nuevoUsuario) throws RemoteException, NoSeEncuentraUsuarioException, ContrasenaInvalidaException {
+		UsuarioDAO daoUsuario = getUsuarioDAO();
+		try {
+			Usuario user=daoUsuario.findByName(usuario);
+			if(user.getClaveB().equals(password)){
+				try {
+					daoUsuario.cambiarNombre(usuario, nuevoUsuario);
+				} catch (NoExisteUsuarioException e) {
+				}
+			}else{
+				throw new ContrasenaInvalidaException();
+			}
+		} catch (NotDataFoundException e) {
+			throw new NoSeEncuentraUsuarioException();
+		}
+
+	}
+
+	public void cambiarPassword(String usuario, String oldPassword, String newPassword) throws RemoteException, NoSeEncuentraUsuarioException, ContrasenaInvalidaException {
+		UsuarioDAO daoUsuario = getUsuarioDAO();
+		try {
+			Usuario user=daoUsuario.findByName(usuario);
+			if(user.getClaveB().equals(oldPassword)){
+				try {
+					daoUsuario.cambiarPassword(usuario, newPassword);
+				} catch (NoExisteUsuarioException e) {
+				}
+			}else{
+				throw new ContrasenaInvalidaException();
+			}
+		} catch (NotDataFoundException e) {
+			throw new NoSeEncuentraUsuarioException();
+		}
+
+
+	}
+
+	public void cobrarSaldo(String usuario, String password, int credito) throws RemoteException, MontoInsuficienteException, NoSeEncuentraUsuarioException, ContrasenaInvalidaException {
+		UsuarioDAO daoUsuario = getUsuarioDAO();
+		try {
+			Usuario user=daoUsuario.findByName(usuario);
+			if(user.getClaveB().equals(password)){
+				if(daoUsuario.creditoSuficiente(credito, user.getIdUsuarioB())){
+					daoUsuario.restarSaldo(credito, user.getIdUsuarioB());
+				}else{
+					throw new MontoInsuficienteException();
+				}
+			}else{
+				throw new ContrasenaInvalidaException();
+			}
+
+		} catch (NotDataFoundException e) {
+			throw new NoSeEncuentraUsuarioException();
+		}
+
+
+	}
+
+	public UsuarioVO login(String usuario, String clave) throws RemoteException, NoSeEncuentraUsuarioException, ContrasenaInvalidaException {
+		UsuarioDAO daoUsuario = getUsuarioDAO();
+		try {
+			Usuario user=daoUsuario.findByName(usuario);
+			if(user.getClaveB().equals(clave)){
+				if(user.getNivelPrivilegioB()==1){
+					UsuarioVO nuevo=new UsuarioVO(user.getUsuarioB());
+					nuevo.setIdUsuario(user.getIdUsuarioB());
+					nuevo.setNombreB(user.getNombreB());
+					nuevo.setApellidoB(user.getApellidoB());
+					return nuevo;
+				}else{
+					throw new NoSeEncuentraUsuarioException();
+				}
+			}else{
+				throw new ContrasenaInvalidaException();
+			}
+		} catch (NotDataFoundException e) {
+			throw new NoSeEncuentraUsuarioException();
+		}
+
+	}
+
+
+	private static UsuarioDAO getUsuarioDAO() {
+		try {
+			return (UsuarioDAO) Class.forName("daoImpl.UsuarioDAODB")
+					.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
