7b17677b7c14374bd40f6d0c62009270e4e194a3
vtuyare91 <vtuyare91@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-18 11:37:26 +0000

    Agrego metodo para cancelar desafio y excepciones no contempladas antes

--- trunk/proyecto/COMM/src/comm/ServiciosDesafio.java
+++ trunk/proyecto/COMM/src/comm/ServiciosDesafio.java
@@ -4,12 +4,18 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import commExceptions.MontoInsuficienteException;
 import commExceptions.NoHayDesafiosDisponiblesException;
 
 public interface ServiciosDesafio extends Remote{
 
+	//metodo que devuelve una lista de desafios
 	public ArrayList<DesafioBatallaNavalVO> getDesafios() throws RemoteException, NoHayDesafiosDisponiblesException;
+	//metodo que permite hacer la espera
 	public boolean aceptaronDesafio(UsuarioVO usuario) throws RemoteException;
-	public void crearDesafio(UsuarioVO usuario,int monto) throws RemoteException;
-	public ArrayList<DesafioVO> getDesafiosLudo() throws RemoteException;
+	//metodo para crear un desafio
+	public void crearDesafio(UsuarioVO usuario,int monto) throws RemoteException, MontoInsuficienteException;
+	//metodo para cancelar desafio
+	public void cancelarDesafio(UsuarioVO usuario) throws RemoteException;
+
 }
