9d4a3386fefff9bd151f8b51a8d94b23974ba4be
vtuyare91 <vtuyare91@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-18 13:43:23 +0000

    se agrega stub admin

--- proyecto/BUS/src/busImpl/BusMain.java
+++ proyecto/BUS/src/busImpl/BusMain.java
@@ -5,8 +5,10 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+import busImpl.administracion.ServiciosAdministradorImpl;
 import busImpl.batallaNaval.ServiciosBatallaNavalImpl;
 
+import comm.ServiciosAdministrador;
 import comm.ServiciosBatallaNaval;
 import comm.ServiciosDesafio;
 import comm.ServiciosRanking;
@@ -18,17 +20,20 @@ public class BusMain {
 	private static Ranking bsRanking;
 	private static Desafio bsDesafio;
 	private static ServiciosBatallaNavalImpl bsBN;
+	private static ServiciosAdministrador bsAdmin;
 
 	public static void main(String[] args) {
 		bs = new BusServer();
 		bsRanking = new Ranking();
 		bsDesafio = new Desafio();
 		bsBN= new ServiciosBatallaNavalImpl();
+		bsAdmin= new ServiciosAdministradorImpl();
 		try {
 			ServiciosUsuario stub = (ServiciosUsuario) UnicastRemoteObject.exportObject(bs, 0);
 			ServiciosRanking stubRanking = (ServiciosRanking) UnicastRemoteObject.exportObject(bsRanking, 0);
 			ServiciosDesafio stubDesafio = (ServiciosDesafio) UnicastRemoteObject.exportObject(bsDesafio, 0);
 			ServiciosBatallaNaval stubBN = (ServiciosBatallaNaval) UnicastRemoteObject.exportObject(bsBN, 0);
+			ServiciosAdministrador stubAdmin = (ServiciosAdministrador) UnicastRemoteObject.exportObject(bsAdmin, 0);
 			try {
 
 				Registry registry = LocateRegistry.createRegistry(1099);
@@ -36,6 +41,7 @@ public class BusMain {
 				registry.rebind("Hello", stub);
 				registry.rebind("Desafio", stubDesafio);
 				registry.rebind("BatallaNavalServices", stubBN);
+				registry.rebind("AdministrationServices", stubAdmin);
 				System.out.println("Servidor remoto registrado OK");
 
 			} catch (Exception e) {
