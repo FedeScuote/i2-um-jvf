729a775b8abea584273d628b05afee9f476a6fd1
john.hirata.personal <john.hirata.personal@gmail.com>
2012-11-18 12:57:58 +0000

    Agregando pruebas unitarias

--- proyecto/DAO/src/junit/RankingUnit.java
+++ proyecto/DAO/src/junit/RankingUnit.java
@@ -3,6 +3,7 @@ package junit;
 import daoImpl.RankingDAODB;
 import excepcionesB.NoHayRankingException;
 import junit.framework.TestCase;
+import busImpl.Ranking;
 
 public class RankingUnit extends TestCase {
 	private RankingDAODB r;
@@ -23,6 +24,21 @@ public class RankingUnit extends TestCase {
 	public void testGetGanadas2(){
 		try {
 			r.getGanadasBatallaNaval(5);
+		} catch (NoHayRankingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void testGetRankingBatallaNaval(){
+		try {
+			assertEquals("Debería dar 6",6,((Ranking)r.getRankingBatallaNaval().get(0)).getGanadas());
+			assertEquals("Se espera un valor correcto, jhirata","jhirata",((Ranking)r.getRankingBatallaNaval().get(0)).getUsuario());
+
+			assertEquals("Debería dar 5",5,((Ranking)r.getRankingBatallaNaval().get(1)).getGanadas());
+			assertEquals("Se espera un valor correcto, fscuoteguazza","fscuoteguazza",((Ranking)r.getRankingBatallaNaval().get(1)).getUsuario());
+
+
 		} catch (NoHayRankingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
