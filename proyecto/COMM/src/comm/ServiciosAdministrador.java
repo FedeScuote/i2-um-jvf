74500ca47705e2525d3c675218db5fce4e224844
vtuyare91 <vtuyare91@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-18 13:45:18 +0000

    Se modifican firmas de metodos admin

--- proyecto/COMM/src/comm/ServiciosAdministrador.java
+++ proyecto/COMM/src/comm/ServiciosAdministrador.java
@@ -1,23 +1,25 @@
 package comm;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import commExceptions.ContrasenaInvalidaException;
 import commExceptions.MontoInsuficienteException;
 import commExceptions.NoSeEncuentraUsuarioException;
+import commExceptions.UsuarioDuplicadoException;
 
 
 
 
 
 
-public interface ServiciosAdministrador {
+public interface ServiciosAdministrador extends Remote{
 
 	public UsuarioVO login(String usuario,String clave) throws RemoteException,NoSeEncuentraUsuarioException, ContrasenaInvalidaException;
-	public void agregarUsuario(String usuario, String clave, int nivelPrilegio, int virtual, String nombre, String apellido, String pais) throws RemoteException,NoSeEncuentraUsuarioException, ContrasenaInvalidaException;
+	public void agregarUsuario(String usuario, String clave, int nivelPrilegio, String nombre, String apellido, String pais) throws RemoteException, UsuarioDuplicadoException;
 	public void cambiarNombre(String usuario,String password, String nuevoUsuario) throws RemoteException,NoSeEncuentraUsuarioException, ContrasenaInvalidaException;
 	public void cambiarPassword(String usuario,String oldPassword, String newPassword) throws RemoteException,NoSeEncuentraUsuarioException, ContrasenaInvalidaException;
 	public void cobrarSaldo(String usuario, String password,int credito) throws RemoteException, MontoInsuficienteException,NoSeEncuentraUsuarioException, ContrasenaInvalidaException;
 	public void acreditarSaldo(String usuario, int credito) throws RemoteException,NoSeEncuentraUsuarioException;
-	
+
 }
