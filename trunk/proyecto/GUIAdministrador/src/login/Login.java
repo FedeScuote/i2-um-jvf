e1de000bc1709c73764f417cea81cdd5c0a451f1
fscuoteguazza <fscuoteguazza@2f0f1fb5-662e-a12d-3d34-65b1b419a192>
2012-11-18 14:25:05 +0000

    implementacion login gui admin

--- trunk/proyecto/GUIAdministrador/src/login/Login.java
+++ trunk/proyecto/GUIAdministrador/src/login/Login.java
@@ -197,7 +197,6 @@ public class Login extends JFrame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				Login thisClass = new Login();
